68f9d3b28142083757a4a2c32851346a018e95fd
andre <andrey.ignatuk567@gmail.com>
2025-07-14 15:32:17 +0200

    add JavaScript interactive and min html

---
+++ src/main.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('show');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.1,
+        rootMargin: '0px 0px -100px 0px'
+    });
+
+
+    document.querySelectorAll('.features-section .hidden, .scanning-section .hidden').forEach(el => {
+        observer.observe(el);
+    });
+});
+
+
+
+
+
+
+
+
